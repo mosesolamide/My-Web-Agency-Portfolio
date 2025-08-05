cce1c78e159a45acfc75857670a19b8bdf3893ca
mosesolamide <olamidemoses132@gmail.com>
2025-08-05 19:57:55 +0100

    new

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,33 +4,33 @@ export default function Home(): JSX.Element {
     return (
         <div>
             <div className='max-w-6xl'>
-                <h1 className='text-5xl md:text-6xl font-medium leading-20 md:w-2xl'>We build professional websites that:</h1>
+                <h1 className='text-4xl sm:text-5xl md:text-6xl font-medium leading-20 md:w-2xl'>We build professional websites that:</h1>
                 <div className='mt-6 flex flex-col lg:flex-row gap-4'>
                     <div 
                         className='border border-white/20 rounded-full 
                         text-center px-4 py-2 bg-[#020617]/20 backdrop-blur-lg 
-                        font-medium text-sm sm:text-lg'
+                        font-medium text-xs sm:text-sm md:text-lg'
                     >
                         🚀 Load FAST
                     </div>
                     <div 
                         className='border border-white/20 rounded-full 
                         text-center px-4 py-2 bg-[#020617]/20 backdrop-blur-lg 
-                        font-medium text-sm sm:text-lg'
+                        font-medium text-xs sm:text-sm md:text-lg'
                     >
                         📈 Rank HIGH on Google
                     </div>
                     <div 
                         className='border border-white/20 rounded-full 
                         text-center px-4 py-2 bg-[#020617]/20 backdrop-blur-lg 
-                        font-medium text-sm sm:text-lg'
+                        font-medium text-xs sm:text-sm md:text-lg'
                     >
                         ✨ User Friendly
                     </div>
                     <div 
                         className='border border-white/20 rounded-full 
                         text-center px-4 py-2 bg-[#020617]/20 backdrop-blur-lg 
-                        font-medium text-sm sm:text-lg'
+                        font-medium text-xs sm:text-sm md:text-lg'
                     >
                         💵 Convert visitors to clients!
                     </div>
